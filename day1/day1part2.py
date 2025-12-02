8d66c63de5858c3f55d0ae7931e2f943c0d817c5
Natalie Wong <wong.lt.natalie@gmail.com>
2025-12-02 12:49:18 -0500

    Clean up directory and code

--- day1/day1part2.py
+++ day1/day1part2.py
@@ -4,7 +4,6 @@
 # Import the puzzle
 with open('day1Input.txt') as puzzleFile:
     puzzle = puzzleFile.read().splitlines()
-print(puzzle)
 puzzleFile.close()
 
 # Start at 50
@@ -13,9 +12,9 @@ password = 0
 
 # Iterate through each rotation and update dial
 for rotation in puzzle:
-    print(rotation)
-
     if rotation[0] == 'L': # L - subtract
+        if (dial == 0):
+            dial = 100
         dial -= int(rotation[1:])
     else: # R - add
         dial += int(rotation[1:])
@@ -23,8 +22,11 @@ for rotation in puzzle:
     # Adjust the dial number if out of range
     while (dial < 0):
         dial += 100
+        password += 1 
     while (dial > 99):
         dial -= 100
+        if (dial != 0): # For part 2 only
+            password += 1 
 
     # Update password (dial at 0 count)
     if (dial == 0):
