1bf015e7f23734a4a389f6f67b10507dcbd90819
nataliewxngg <wong.lt.natalie@gmail.com>
2026-01-01 18:37:21 -0500

    Attempt to solve using new interior intersection logic

--- day9/day9part2.py
+++ day9/day9part2.py
@@ -13,57 +13,66 @@ with open(os.path.join(here, 'day9Input.txt')) as puzzleFile:
         puzzle.append((int(line[:line.find(',')]), int(line.strip('\n')[line.find(',')+1:])))
 
 puzzleFile.close()
+print(puzzle)
+
+# Collect the vertices for the outline green tiles
+edges = []
+for indexOfPt in range(len(puzzle)):
+    curr = puzzle[indexOfPt]
+    next = puzzle[(indexOfPt + 1) % len(puzzle)]
+
+    edges.append((curr[0], curr[1], next[0], next[1]))
+
+print(edges)
 
 # Determines the area given two points (of red tiles)
 def getArea(pt1, pt2):
     return (pt1[0]-pt2[0]+1) * (pt1[1]-pt2[1]+1)
 
-# Collects all the outline green tiles
-outlineGreenTiles = []
-for indexOfPt in range(len(puzzle)):
-    indexOfOtherPt = (indexOfPt + 1) % len(puzzle)
+# Returns a and b such that a is always less than b
+def sort(a, b):
+    return (a, b) if a < b else (b,a)
 
-    x1, y1 = puzzle[indexOfPt]
-    x2, y2 = puzzle[indexOfOtherPt]
-    
-    if x1 == x2:
-        if (y1 > y2):
-            y1, y2 = y2, y1
+# Calculate the manhatten distance between 2 points
+def manhattan(pt1, pt2):
+    return abs(pt1[0] - pt2[0]) + abs(pt1[1] - pt2[1])
 
-        for i in range(y1+1, y2):
-            outlineGreenTiles.append((x1, i))
-    else:
-        if (x1 > x2):
-            x1, x2 = x2, x1
+# Return true if the rectangle crosses the boundary
+def rectangleIntersectsEdge(minx, miny, maxx, maxy, edges):
+    for edge in edges:
+        x1, y1, x2, y2 = edge
 
-        for i in range(x1+1, x2):
-            outlineGreenTiles.append((i, y1))
+        edgeMinX, edgeMaxX = sort(x1, x2)
+        edgeMinY, edgeMaxY = sort(y1, y2)
 
-# Collects and add the filling green tiles as well
-validTiles = sorted(outlineGreenTiles + puzzle, key=lambda tup: (tup[1], tup[0]))
-newTiles = []
-for indexOfPt in range(len(validTiles)):
-    indexOfOtherPt = (indexOfPt + 1) % len(validTiles)
+        if (minx < edgeMaxX and maxx > edgeMinX and miny < edgeMaxY and maxy > edgeMinY):
+            return True
+    return False
 
-    x1, y1 = validTiles[indexOfPt]
-    x2, y2 = validTiles[indexOfOtherPt]
 
-    # find where y changes
-    if y1 == y2 and abs(x1 - x2) != 1:
-            for i in range(min(x1, x2)+1, min(x1,x2) + abs(x2-x1)):
-                if (i, y1) not in validTiles:
-                    newTiles.append((i, y1))
-
-validTiles = validTiles + newTiles
-
+# Calculate the greatest area
 greatestArea = 0
+
 for indexOfPt1 in range(len(puzzle)):
-    for indexOfPt2 in range(indexOfPt1+1, len(puzzle)):
+    for indexOfPt2 in range(indexOfPt1, len(puzzle)):
 
-        pt3 = (puzzle[indexOfPt1][0], puzzle[indexOfPt2][1])
-        pt4 = (puzzle[indexOfPt2][0], puzzle[indexOfPt1][1])
+        pt1 = puzzle[indexOfPt1]
+        pt2 = puzzle[indexOfPt2]
 
-        if getArea(puzzle[indexOfPt1], puzzle[indexOfPt2]) > greatestArea and (pt3 in validTiles or pt3 in puzzle) and (pt4 in validTiles or pt4 in puzzle):
-            greatestArea = getArea(puzzle[indexOfPt1], puzzle[indexOfPt2])
+        minx, maxx = sort(pt1[0], pt2[0])
+        miny, maxy = sort(pt1[1], pt2[1])
+
+        # Optimize solution by skipping small rectangles
+        manhattanD = manhattan(pt1, pt2)
+        if manhattanD**2 <= greatestArea:
+            continue
+        
+        # Reject if rectangle crosses the boundary
+        if rectangleIntersectsEdge(minx, miny, maxx, maxy, edges):
+            continue
+
+        area = getArea(pt1, pt2)
+        if area > greatestArea:
+            greatestArea = area
 
 print(greatestArea)
